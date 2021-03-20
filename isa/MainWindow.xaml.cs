fc3567b20ff91bf0f8d510fade6fa108eca77aea
hubert zablocki <hubert.zablocki@bcoders.pl>
2021-03-20 19:00:15 +0100

    lab03

--- isa/MainWindow.xaml.cs
+++ isa/MainWindow.xaml.cs
@@ -35,20 +35,70 @@ namespace isa
             var d = decimal.Parse(D.Text, CultureInfo.InvariantCulture);
             var n = int.Parse(N.Text);
             var manager = new NumberFormatService(a, b, d);
-            List<XRepresentation> Values = new();
+
+
+            List<TableRow> Values = new();
             for (int i = 0; i < n; i++)
             {
-                var value = new XRepresentation();
+                var value = new TableRow();
                 value.Index = i + 1;
                 value.GeneratedXReal = manager.RandomDecimal();
-                value.XIntFromGeneratedXReal = manager.RealToInt(value.GeneratedXReal);
-                value.XBinFromXInt = manager.IntToBin(value.XIntFromGeneratedXReal);
-                value.XIntFromXBin = manager.BinToInt(value.XBinFromXInt);
-                value.XRealFromXInt = manager.IntToReal(value.XIntFromXBin);
-                value.XFunction = manager.CalculateFx(value.XRealFromXInt);
+                value.Fx = manager.CalculateFx(value.GeneratedXReal);
 
                 Values.Add(value);
             }
+
+
+            var fxMin = Values.Select(_ => _.Fx).Min();
+
+            // gx
+            Values.ForEach(v =>
+            {
+                v.Gx = v.Fx - fxMin + manager.D;
+            });
+
+            // p
+            var gxSum = Values.Select(v => v.Gx).Sum();
+
+            Values.ForEach(v =>
+            {
+                v.P = v.Gx / gxSum;
+            });
+
+
+            // q
+            for (int i = 0; i < Values.Count; i++)
+            {
+                if(i == Values.Count - 1)
+                {
+                    Values[i].Qx = 1;
+                } else
+                {
+                    if (i > 0)
+                    {
+                        Values[i].Qx = Values[i].P + Values[i - 1].Qx;
+                    }
+                    else
+                    {
+                        Values[i].Qx = Values[i].P;
+                    }
+                }
+            }
+
+            // r 
+            Values.ForEach(_ =>
+            {
+                _.R = Convert.ToDecimal(new Random().NextDouble());
+            });
+
+            for (int i = 0; i < Values.Count; i++)
+            {
+                Values[i].XRel = Values.First(_ => _.Qx > Values[i].R).GeneratedXReal;
+            }
+
+
+
+
             ValuesListView.ItemsSource = Values;
         }
     }
---
+++ isa/Models/TableRow.cs
@@ -0,0 +1,14 @@
+﻿namespace isa
+{
+    public class TableRow
+    {
+        public int Index { get; set; }
+        public decimal GeneratedXReal { get; set; }
+        public decimal Fx { get; set; }
+        public decimal Gx { get; set; }
+        public decimal P { get; set; }
+        public decimal Qx { get; set; }
+        public decimal R { get; set; }
+        public decimal XRel { get; set; }
+    }
+}
